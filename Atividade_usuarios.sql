ecf476e948a9a4d88a30e8ea61106d1b3645e23d
Danielle Gonçalves Ferreira <160173090+daniferreira05@users.noreply.github.com>
2024-05-21 08:32:26 -0300

    Add files via upload

--- Atividade_usuarios.sql
+++ Atividade_usuarios.sql
@@ -14,6 +14,12 @@ SELECT p.ID_POST, p.TEXTO, p.DATA_HORA, nome_usuario
 FROM postagens as p
 join usuarios as u on p.usuario = u.id_usuario;
 
+#5. Listar o nome dos usuários e a quantidade de postagens que cada um fez.
+select concat_ws(' ', u.nome_usuario, u.sobrenome) as nome_usuario,
+count(id_post) as qntd_post
+from postagens as p right join usuarios as u 
+on u.ID_USUARIO = p.USUARIO
+group by USUARIO;
 
 
 
